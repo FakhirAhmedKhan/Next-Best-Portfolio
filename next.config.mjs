11dc5be42fcecda1a58532d5d2182aeaaee01311
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-10-27 00:41:26 +0400

    Initial portfolio build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 👈 Enables static HTML export
   images: {
+    unoptimized: true, // 👈 Required for GitHub Pages (no Next Image Optimization)
     remotePatterns: [
       {
         protocol: 'https',
@@ -8,6 +10,8 @@ const nextConfig = {
       },
     ],
   },
+  basePath: '/Next-Best-Portfolio', // 👈 your repo name
+  assetPrefix: '/Next-Best-Portfolio/', // 👈 same as basePath
 };
 
 export default nextConfig;
