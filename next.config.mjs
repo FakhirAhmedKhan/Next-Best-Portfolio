b6f13de7b412c53ce8bab299a5dacb2e5977ae0b
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-10-27 09:51:59 +0400

    final plzy deplay😫

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Enables static HTML export
   images: {
-    unoptimized: true, // GitHub Pages can't handle Next.js image optimization
     remotePatterns: [
       {
         protocol: 'https',
@@ -10,9 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  basePath: '/Next-Best-Portfolio', // ✅ Your GitHub repo name
-  assetPrefix: '/Next-Best-Portfolio/', // ✅ Same as basePath
-  trailingSlash: true, // ✅ Recommended for GitHub Pages to prevent routing 404s
 };
 
 export default nextConfig;
--- services/endpoints.js
+++
@@ -1,6 +0,0 @@
-export const API_ENDPOINTS = {
-  skills: '/app/api/data/skillsIcons',
-  socialLinks: '/api/data/socialLinks',
-  education: '/api/data/educationData',
-  projects: '/api/data/projectsData',
-};
