6561b49e89a232624e82916c94ad4c49395cadbe
Fakhir Ahmed Khan <fakhirahmed898@gmail.com>
2025-10-31 17:20:38 +0400

    add luggung

--- components/SKILLS/components/SkillsSection.jsx
+++ components/SKILLS/components/SkillsSection.jsx
@@ -6,20 +6,22 @@ import { HeadIng } from "@/UI/Head";
 import { Badge } from "@/UI/Badge";
 import { Zap } from "lucide-react";
 import { SectionSTyle } from "@/UI/motionConfige";
-import { useAppContext } from "@/lib/contexts/app-context";
+import { useLanguage } from "@/lib/contexts/language-context";
 
 export default function SkillsSection() {
-  const { sectionTitles } = useAppContext();
+  const { data } = useLanguage();
 
+  const badgeText = data.BageName?.SkillBade || "Default Badge";
+  const Data = data.sectionTitles?.skillsSection || {};
   return (
     <section id="skills" className={SectionSTyle}>
 
       <div className="relative max-w-7xl mx-auto">
 
-        <HeadIng Tittle={sectionTitles.skillsSection?.title} Pragaphic={sectionTitles.skillsSection?.paragraph} />
+        <HeadIng Tittle={Data.title} Pragaphic={Data.paragraph} />
 
         <div className="flex justify-center items-center mb-6">
-          <Badge Icon={Zap} BageName={sectionTitles.BageName?.SkillBage} className="px-4 py-2 text-sm" />
+          <Badge Icon={Zap} BageName={badgeText} className="px-4 py-2 text-sm" />
         </div>
         <Card />
         <Bottomdecoration />
